8210886173787967fb04c369a8194bd393e17373
Rishthegreat <rishitarora2005@gmail.com>
2024-01-23 23:27:25 -0500

    localstorage for usertoken

--- frontend/auth-context.js
+++ frontend/auth-context.js
@@ -1,21 +1,44 @@
 /* eslint-disable */
 import {createContext, useEffect, useState} from "react";
+import {MMKV} from "react-native-mmkv";
 
 export const AuthContext = createContext()
-
+const storage = new MMKV()
 export const AuthProvider = ({children}) => {
     const [userToken, setUserToken] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
     const login = () => {
         setIsLoading(true)
         setUserToken('asdkfadfasdfkjh') //later use actual token
+        storage.set('userToken', 'asdkfadfasdfkjh')
         setIsLoading(false)
     }
     const logout = () => {
         setIsLoading(true)
         setUserToken(null)
+        storage.delete('userToken')
         setIsLoading(false)
     }
+
+    const IsLoggedIn = () => {
+        try {
+            setIsLoading(true)
+            if (storage.contains('userToken')) {
+                let userToken = storage.getString('userToken')
+                setUserToken(userToken)
+            } else {
+                setUserToken(null)
+            }
+            setIsLoading(false)
+        } catch (e) {
+            setIsLoading(false)
+            console.log(`error from is logged in ${e}`)
+        }
+    }
+
+    useEffect(() => {
+        IsLoggedIn()
+    }, []);
 
     return (
         <AuthContext.Provider value={{login, logout, userToken}}>
